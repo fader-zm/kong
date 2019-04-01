25e407f8c3e0f36964fff5db28448c65651c26e4
zhaomeng <zhaomeng@163.com>
2019-04-01 21:44:56 +0800

    发送短信数值定义常量

--- meiduo_mell/meiduo_mell/apps/verifications/views.py
+++ meiduo_mell/meiduo_mell/apps/verifications/views.py
@@ -7,6 +7,7 @@ import logging
 from rest_framework import status
 
 from meiduo_mell.libs.yuntongxun.sms import CCP
+from . import constants
 
 logger = logging.getLogger('django')
 # Create your views here.
@@ -27,12 +28,12 @@ class SMSCodeView(APIView):
         logger.info(sms_code)
         
         # 5. 将验证码保存到redis数据库
-        redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
         # 6. 存储一个标记, 表示此手机号已发送短信验证码 标记
-        redis_conn.setex('send_flag_%s' % mobile, 60, 1)
+        redis_conn.setex('send_flag_%s' % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
         # 7. 发送容联云通信发送短信验证码
         # CCP().send_template_sms(手机号, [验证码, 时间], 模板)
-        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES//6], 1)
         # 8. 响应
         return Response({'message': 'ok'})
     
