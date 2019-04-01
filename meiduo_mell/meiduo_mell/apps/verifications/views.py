654a8fc0e6e47f19866807a990d54a09e0532f43
zhaomeng <zhaomeng@163.com>
2019-04-01 19:52:18 +0800

    短信验证码逻辑简单实现

--- meiduo_mell/meiduo_mell/apps/verifications/views.py
+++ meiduo_mell/meiduo_mell/apps/verifications/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from random import randint
+from django_redis import get_redis_connection
+from rest_framework.response import Response
+import logging
 
+from meiduo_mell.libs.yuntongxun.sms import CCP
+
+logger = logging.getLogger('django')
 # Create your views here.
 
 
@@ -8,11 +15,18 @@ class SMSCodeView(APIView):
     
     def get(self, request, mobile):
         # 1. 生成验证码
+        sms_code = '%06d' % randint(0, 999999)
+        logger.info(sms_code)
         # 2. 创建redis连接对象
+        redis_conn = get_redis_connection('verify_codes')
         # 3. 将验证码保存到redis数据库
+        redis_conn.setex('sms_%s' % mobile, 300, sms_code)
         # 4. 发送容联云通信发送短信验证码
+        # CCP().send_template_sms(手机号, [验证码, 时间], 模板)
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
         # 5. 响应
-        pass
+        return Response({'message': 'ok'})
+    
         
         
 
